d3edfeedde22d798620b464f4b91a412ecd0073e
sujiiiin <sujean2002@naver.com>
2025-07-20 21:04:25 +0900

    Update run_sbom_1.sh

--- components/scripts/run_sbom_1.sh
+++ components/scripts/run_sbom_1.sh
@@ -165,5 +165,5 @@ done
 
 # CVSS 9 이상 정책 검사
 echo "📤 CVSS 9 이상 정책 검사 중..."
-python3 /home/ec2-user/check_cvss_and_notify.py "$PROJECT_UUID" "$DT_API_KEY" "http://localhost:8080" "$REPO_NAME" "$PROJECT_VERSION" > /tmp/cvss.log 2>&1
+(cd /home/ec2-user && python3 check_cvss_and_notify.py "$PROJECT_UUID" "$DT_API_KEY" "http://localhost:8080" "$REPO_NAME" "$PROJECT_VERSION") > /tmp/cvss.log 2>&1
 
